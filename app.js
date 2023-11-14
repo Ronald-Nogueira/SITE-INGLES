00714822c1401f149242af572f5a36e2aa8835bc
Ronald-Nogueira <93053548+Ronald-Nogueira@users.noreply.github.com>
2023-11-13 23:02:54 -0300

    d

--- app.js
+++ app.js
@@ -2,3 +2,27 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+
+
+
+
+function openModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.classList.add('active');
+}
+
+function closeModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.classList.remove('active');
+}
+
+window.onclick = function (event) {
+  var modals = document.getElementsByClassName("modal");
+  for (var i = 0; i < modals.length; i++) {
+    var modal = modals[i];
+    if (event.target == modal) {
+      modal.classList.remove('active');
+    }
+  }
+};

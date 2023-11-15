67421774d79fe6ea4f41905b6a4922bbff41f042
Ronald-Nogueira <93053548+Ronald-Nogueira@users.noreply.github.com>
2023-11-15 01:54:26 -0300

    f

--- app.js
+++ app.js
@@ -5,24 +5,30 @@ hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
 
 
-
-
 function openModal(modalId) {
   var modal = document.getElementById(modalId);
   modal.style.display = "block";
+  setTimeout(function() {
+      modal.style.opacity = "1";
+  }, 50); // Adiciona um pequeno atraso para a transição ser notada
 }
 
 function closeModal(modalId) {
   var modal = document.getElementById(modalId);
-  modal.style.display = "none";
+  modal.style.opacity = "0";
+  setTimeout(function() {
+      modal.style.display = "none";
+  }, 300); // Tempo da transição
 }
 
-window.onclick = function (event) {
-  var modals = document.getElementsByClassName("modal");
-  for (var i = 0; i < modals.length; i++) {
-    var modal = modals[i];
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+// Fecha a janela modal se clicar fora dela
+window.onclick = function(event) {
+  if (event.target.classList.contains('modal')) {
+      event.target.style.opacity = "0";
+      setTimeout(function() {
+          event.target.style.display = "none";
+      }, 300);
   }
-};
+}
+
+
